e3f20870d2ce99a04bee201325b1478a343c09a4
fwornle <fwornle@yahoo.co.uk>
2021-09-13 14:02:33 +0200

    welcome fragment and instructions fragment completed and hooked up to navController

--- starter/app/src/main/java/com/udacity/shoestore/InstructionFragment.kt
+++ starter/app/src/main/java/com/udacity/shoestore/InstructionFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
 import com.udacity.shoestore.databinding.FragmentInstructionBinding
 
 class InstructionFragment : Fragment() {
@@ -16,6 +17,10 @@ class InstructionFragment : Fragment() {
         val binding: FragmentInstructionBinding = DataBindingUtil.inflate<FragmentInstructionBinding>(
             inflater, R.layout.fragment_instruction, container, false)
 
+        // hook up button to navController
+        binding.btShoeList.setOnClickListener { view: View ->
+            Navigation.findNavController(view).navigate(R.id.action_instructionFragment_to_shoeListFragment)
+        }
 
         // return View object
         return binding.root
--- starter/app/src/main/java/com/udacity/shoestore/WelcomeFragment.kt
+++ starter/app/src/main/java/com/udacity/shoestore/WelcomeFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
 import com.udacity.shoestore.databinding.FragmentWelcomeBinding
 
 class WelcomeFragment : Fragment() {
@@ -16,6 +17,10 @@ class WelcomeFragment : Fragment() {
         val binding: FragmentWelcomeBinding = DataBindingUtil.inflate<FragmentWelcomeBinding>(
             inflater, R.layout.fragment_welcome, container, false)
 
+        // hook up button to navController
+        binding.btInstructions.setOnClickListener { view: View ->
+            Navigation.findNavController(view).navigate(R.id.action_welcomeFragment_to_instructionFragment)
+        }
 
         // return View object
         return binding.root
